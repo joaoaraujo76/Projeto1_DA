cfb7d6466f08e28944bc7731c3201d60e89c0a59
Tiago <t.branquinho.slb@gmail.com>
2022-04-12 18:25:53 +0100

    Express Orders out of Order File

--- include/App.h
+++ include/App.h
@@ -20,20 +20,24 @@ private:
     std::vector <Van> vans;
     std::vector <std::string> filesname = {"orders.txt", "vans.txt", "express_deliveries.txt"};
     int workTime = 8;
+    int maxExpressDuration = 4;
 public:
     App();
     ~App();
     void readFile(int file);
+    void clearFile(std::fstream *file, const int FILE_NUM);
     void readVans();
     void readOrders();
     std::vector<std::string> readExpressOrders();
     void writeExpressOrders(int time, const std::vector<Order>& orders);
+    void updateOrders();
     void loadData();
     void optimizeExpressDeliveries();
     std::vector<Van> &getVans();
     std::vector<Order> &getOrders() ;
     int getWorkingTime() const;
     void setWorkingTime(int workingTime);
+    void setMaxExpressDuration(int maxExpressDuration);
 };
 
 #endif //PROJETO1_DA_APP_H
